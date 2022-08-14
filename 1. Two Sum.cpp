877087cd693e8960023ed4fbc6f2b0ae8a83107c
Rahul Dohare <67474480+rahuldohare007@users.noreply.github.com>
2022-08-14 12:31:37 +0530

    Update and rename 1. Two Sum to 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,28 +1,23 @@
+/*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
 
- 
-
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
 
+Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
-Example 3:
 
+Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
+*/
 
-
-
-1. Using for loops
+//1. Using for loops
 
 #include <iostream> 
 #include <vector>
@@ -49,9 +44,9 @@ public:
     }    
 };
 
-O(n2) time complexity
+//O(n2) time complexity
 
-2. Using Hashing 
+//2. Using Hashing 
 
 #include <iostream> 
 #include <vector>
@@ -85,4 +80,4 @@ public:
     }     
 };
 
-O(n) time complexity
+//O(n) time complexity
