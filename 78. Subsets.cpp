8161d31bae27659c2e2b38b0f91c9edef66f8ae8
Rahul Dohare <67474480+rahuldohare007@users.noreply.github.com>
2022-08-14 12:37:47 +0530

    Update and rename 78. Subsets to 78. Subsets.cpp

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -1,17 +1,16 @@
+/*
 Given an integer array nums of unique elements, return all possible subsets (the power set).
 
 The solution set must not contain duplicate subsets. Return the solution in any order.
 
- 
-
 Example 1:
-
 Input: nums = [1,2,3]
 Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-Example 2:
 
+Example 2:
 Input: nums = [0]
 Output: [[],[0]]
+*/
 
 class Solution {
 private:
@@ -29,7 +28,7 @@ private:
         } 
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int> > ans;
+        vector<vector<int>> ans;
         vector<int> output;
         int index=0;
         solve(nums,output,index,ans);
