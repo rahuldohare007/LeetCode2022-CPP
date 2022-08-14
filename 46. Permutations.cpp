2fc930f3520ae5c0b69e3430571c9778752f944e
Rahul Dohare <67474480+rahuldohare007@users.noreply.github.com>
2022-08-14 12:36:02 +0530

    Update and rename 46. Permutations to 46. Permutations.cpp

--- 46. Permutations.cpp
+++ 46. Permutations.cpp
@@ -1,18 +1,18 @@
+/*
 Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
 Example 1:
-
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-Example 2:
 
+Example 2:
 Input: nums = [0,1]
 Output: [[0,1],[1,0]]
-Example 3:
 
+Example 3:
 Input: nums = [1]
 Output: [[1]]
-
+*/
 
 class Solution {
 private:
@@ -31,10 +31,9 @@ private:
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-    vector<vector<int>> ans;
-    int index = 0;
-    solve(nums,ans,index);
-    return ans;
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums,ans,index);
+        return ans;
     }
-    
 };
