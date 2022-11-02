bda4c64e18430c0a7d571779a8564f7ead2f3281
Rahul Dohare <67474480+rahuldohare007@users.noreply.github.com>
2022-11-02 22:18:29 +0530

    Create 433. Minimum Genetic Mutation.cpp

---
+++ 433. Minimum Genetic Mutation.cpp
@@ -0,0 +1,62 @@
+/*
+A gene string can be represented by an 8-character long string, with choices from 'A', 'C', 'G', and 'T'.
+Suppose we need to investigate a mutation from a gene string start to a gene string end where one mutation is defined as one single character changed in the gene
+string.
+For example, "AACCGGTT" --> "AACCGGTA" is one mutation.
+There is also a gene bank bank that records all the valid gene mutations. A gene must be in bank to make it a valid gene string.
+Given the two gene strings start and end and the gene bank bank, return the minimum number of mutations needed to mutate from start to end. If there is no such a
+mutation, return -1.
+Note that the starting point is assumed to be valid, so it might not be included in the bank.
+
+Example 1:
+Input: start = "AACCGGTT", end = "AACCGGTA", bank = ["AACCGGTA"]
+Output: 1
+
+Example 2:
+Input: start = "AACCGGTT", end = "AAACGGTA", bank = ["AACCGGTA","AACCGCTA","AAACGGTA"]
+Output: 2
+
+Example 3:
+Input: start = "AAAAACCC", end = "AACCCCCC", bank = ["AAAACCCC","AAACCCCC","AACCCCCC"]
+Output: 3
+ 
+Constraints:
+start.length == 8
+end.length == 8
+0 <= bank.length <= 10
+bank[i].length == 8
+start, end, and bank[i] consist of only the characters ['A', 'C', 'G', 'T'].
+*/ 
+
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        queue<string> q;
+        unordered_map<string, int> vis;
+        int steps = 0;
+        q.push(start);
+        vis[start] = 1;
+
+        while (!q.empty()) {
+            for (int i = q.size(); i > 0; i--) {
+                string s = q.front();
+                q.pop();
+                if (s == end) 
+                    return steps;
+                for (int j = 0; j < 8; j++) {
+                    char oc = s[j];
+                    for (int k = 0; k < 4; k++) {
+                        s[j] = "ACGT"[k];
+                        if (!vis[s] && find(bank.begin(), bank.end(), s) != bank.end()) {
+                            q.push(s);
+                            vis[s] = 1;
+                        }
+                    }
+                    s[j] = oc;
+                }
+            }
+            steps += 1;
+        }
+        return -1;
+    }
+};
